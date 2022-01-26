4fbce2b84eb9dcbe1cb1db605733e2efae91c198
Justin-mario <79143260+Justin-mario@users.noreply.github.com>
2022-01-27 00:15:23 +0100

    interface

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+type notifier interface {
+	notify()
+}
+type user struct {
+	name  string
+	email string
+}
+
+func (u *user) notify() {
+	fmt.Printf("Sending user email to %s<%s>\n",
+		u.name,
+		u.email)
+}
+
+type admin struct {
+	user
+	level string
+}
+
+func main() {
+
+	ad := admin{
+		user: user{
+			name:  "john smith",
+			email: "john@yahoo.com",
+		},
+		level: "super",
+	}
+
+	sendNotification(&ad)
+}
+
+func sendNotification(n notifier) {
+	n.notify()
+}
